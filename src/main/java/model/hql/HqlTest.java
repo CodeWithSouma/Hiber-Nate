3b3eef904e738274bc8c29dd541c842f9a3519ea
CodeWithSouma <souma.hitech@gmail.com>
2021-12-04 20:47:57 +0530

    Complete hql part2

--- src/main/java/model/hql/HqlTest.java
+++ src/main/java/model/hql/HqlTest.java
@@ -18,11 +18,29 @@ public class HqlTest {
         SessionFactory sessionFactory = configuration.buildSessionFactory(registry);
         Session session1 =  sessionFactory.openSession();
         Transaction transaction1 = session1.beginTransaction();
-        Query query = session1.createQuery("FROM Employee WHERE point >= 50");
+        /*Query query = session1.createQuery("FROM Employee WHERE point >= 50");
         List<Employee> employeeList = query.list();
         for (Employee e:employeeList)
             System.out.println(e);
-        System.out.println("Total Student: "+employeeList.size());
+        System.out.println("Total Student: "+employeeList.size());*/
+
+      /*Query query = session1.createQuery("SELECT empId, name, point FROM Employee  WHERE empId=1");
+        Object[] result = (Object[]) query.uniqueResult();
+        for(Object obj : result)
+            System.out.println(obj);*/
+
+       /* Query query = session1.createQuery("SELECT empId, name, point FROM Employee");
+        List<Object[]> tableOfRecords = (List<Object[]>) query.list();
+        for (Object[] row: tableOfRecords) {
+            System.out.println(row[0]+" "+row[1]+" "+row[2]);
+        }*/
+
+        int point = 50;
+        Query query = session1.createQuery("SELECT SUM(point) FROM Employee WHERE point > :point");
+        query.setParameter("point",point);
+        long totalPoint = (long) query.uniqueResult();
+        System.out.println("Total point: "+totalPoint);
+        
         transaction1.commit();
         session1.close();
     }
